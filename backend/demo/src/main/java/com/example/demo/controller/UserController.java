c2ce1573dd9fe961f3dfee7b54f3512a9f61b501
sanskarkayastha <sanskarkayastha750@gmail.com>
2025-10-17 11:15:51 +0545

    Connected database successfully

---
+++ backend/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
