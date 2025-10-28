9dc4a46a70f1e5bd6d16c58eb530e4911dbf1fa5
sanskarkayastha <sanskarkayastha750@gmail.com>
2025-10-28 17:33:53 +0545

    add business data to database

--- frontend/src/app/users/business/page.tsx
+++ frontend/src/app/users/business/page.tsx
@@ -1,9 +1,26 @@
 import RegistrationForm from '../business/components/RegistrationForm';
 
 export default function BusinessPage() {
+
+  async function registerBusiness(formData:any) {
+    'use server';
+    console.log("here")
+    try {
+      const response = await fetch('http://localhost:8080/api/business/addBusiness',{
+        method: 'POST',
+        body: JSON.stringify(formData)
+      })
+      const result = await response.text()
+      console.log(result)
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+  
   return (
     <div>
-      <RegistrationForm />
+      <RegistrationForm onsubmit={registerBusiness}/>
     </div>
   );
 }
