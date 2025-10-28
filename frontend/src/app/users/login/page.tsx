9debe918611503517e0204e005e354aaea534609
eira112 <anushkabhaju@gmail.com>
2025-10-28 13:54:29 +0545

    add login page and home page

--- frontend/src/app/users/login/page.tsx
+++ frontend/src/app/users/login/page.tsx
@@ -1,37 +1,5 @@
-"use client";
-import React, { useState } from 'react'
+import LoginForm from './LoginForm';
 
-const Login = () => {
-    const [formData, setFormData] = useState({ email: "" , password: ""});
-    const [message, setMessage] = useState("");
-    
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const request = await fetch("http://localhost:8080/api/users/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(formData),
-        })
-        const result = await request.text();
-        setMessage(result);
-    }
-  return (
-    <>
-        <div>
-            <h2>Login Page</h2>
-            <p>{message}</p>
-            <form method='Post' onSubmit={handleSubmit}>
-                <input type='email' name='email' value={formData.email} onChange={handleChange}></input>
-                <input type='password' name='password' value={formData.password} onChange={handleChange}></input>
-                <button type='submit'>Login</button>
-            </form>
-        </div>
-    </>
-  )
+export default function LoginPage() {
+  return <LoginForm />;
 }
-
-export default Login
