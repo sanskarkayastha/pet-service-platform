d17a4252cfd62702fd0a3ed10cbd20c2d4f23ccf
sanskarkayastha <sanskarkayastha750@gmail.com>
2026-01-31 13:11:46 +0545

    fixed issue where category was going null

--- backend/demo/src/main/java/com/example/demo/dto/BusinessResponseDTO.java
+++ backend/demo/src/main/java/com/example/demo/dto/BusinessResponseDTO.java
@@ -1,15 +1,18 @@
 package com.example.demo.dto;
 
+import java.util.List;
+
 public record BusinessResponseDTO(
-        String userId,
-        String businessName,
-        String ownerName,
-        String email,
-        String contactNumber,
-        String businessAddress,
-        String description,
-        String city,
-        String panNumber,
-        String imageUrl) {
+                String userId,
+                String businessName,
+                String ownerName,
+                String email,
+                String contactNumber,
+                String businessAddress,
+                String description,
+                String city,
+                String panNumber,
+                List<String> category,
+                String imageUrl) {
 
 }
