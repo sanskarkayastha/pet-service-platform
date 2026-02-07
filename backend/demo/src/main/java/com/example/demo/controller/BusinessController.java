e05caedf680bdac55e89255ab90715ad471e9374
sanskarkayastha <sanskarkayastha750@gmail.com>
2026-02-07 12:42:23 +0545

    status api added

--- backend/demo/src/main/java/com/example/demo/controller/BusinessController.java
+++ backend/demo/src/main/java/com/example/demo/controller/BusinessController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
